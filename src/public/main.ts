d6999efa9895609ed4e6235489af8b51ea543702
39sho <g.39sho@gmail.com>
2023-05-18 12:04:23 +0900

    fix duplication

--- src/public/main.ts
+++ src/public/main.ts
@@ -7,7 +7,7 @@ if (textInput == null || sendBtn == null) {
     throw new Error('something happened in html');
 }
 
-sendBtn.addEventListener('click', async (e) => {
+const send = async () => {
     const sendData = {
         isViewOpen: false,
         room: params.get('room') ?? '',
@@ -23,24 +23,14 @@ sendBtn.addEventListener('click', async (e) => {
     console.log(await res.text());
 
     textInput.value = '';
+};
+
+sendBtn.addEventListener('click', async () => {
+    await send();
 });
 
 addEventListener('keydown', async (e) => {
     if (e.key === 'Enter') {
-        const sendData = {
-            isViewOpen: false,
-            room: params.get('room') ?? '',
-            data: textInput.value
-        };
-        const res = await fetch('/post', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(sendData)
-        });
-        console.log(await res.text());
-
-        textInput.value = '';
+        await send();
     }
 });
