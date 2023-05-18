14c2704aa46aed529a1b3cdc667503aed225b50a
39sho <g.39sho@gmail.com>
2023-05-18 11:35:09 +0900

    rm dist and node_modules

--- dist/public/view/ws-view.js
+++
@@ -1,8 +0,0 @@
-const params = new URL(window.location.href).searchParams;
-export const connect = (callback) => {
-    const source = new EventSource(`/events?room=${params.get('room') ?? ''}`);
-    source.addEventListener('message', e => {
-        callback(e.data);
-        console.log(e.data);
-    });
-};
